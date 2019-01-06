74dd6525abd9d470c91d95beaa386edf947a3025
Victor Salvaya Windows <victorsalvaya@hotmail.com>
2019-01-06 19:25:15 -0200

    continua

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$('.menu-abrir').click(function(){
+ 
+ document.documentElement.classList.add('menu-ativo');
+});
+
+
+$('.menu-fechar').click(function(){
+ 
+document.documentElement.classList.remove('menu-ativo');
+
+});
